22227bbe79b218b9e1450fb69680fe9108715125
Drew Mercer <japanexpress@gmail.com>
2018-03-25 11:58:08 -0400

    Adding auth0 details

--- src/Auth.js
+++ src/Auth.js
@@ -1,2 +1,12 @@
 import auth0 from 'auth0-js';
 
+export default class Auth {
+  auth0 = new auth0.WebAuth({
+    domain: 'pedal-head.auth0.com',
+    clientID: 'E5lu3QF33PKVrxu17dFdJqDmuWz0ylK0',
+    redirectUri: 'http://localhost:3000/callback',
+    audience: 'https://pedal-head.auth0.com/userinfo',
+    responseType: 'token id_token',
+    scope: 'openid'
+  });
+}
