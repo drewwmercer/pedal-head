197df9a21f733c2751d29ee3e90d67219e9ed088
Drew Mercer <japanexpress@gmail.com>
2018-03-25 12:26:38 -0400

    Verifying user is authenticated and hiding login

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,12 +9,15 @@ export default class Main extends Component {
           Do you want to see the secret area? <a href="/secret">Click here</a>
         </p>
 
+{/* Only showing the login button if user is not already authenticated */}
+        {!this.props.auth.isAuthenticated() &&
         <div>
             <hr/>
             Please login to begin.
             <hr/>
             <button onClick={this.props.auth.login}>Login</button>
             </div>
+        }
       </div>
     );
   }
