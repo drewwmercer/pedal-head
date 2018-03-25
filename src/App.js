80ae915882a5cc5997182c9b97d873409c657080
Drew Mercer <japanexpress@gmail.com>
2018-03-25 13:43:48 -0400

    Adding pedalhead details and css starter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './bicycle-wheel-png.png';
 import './App.css';
 import Main from './components/Main';
 import Secret from './components/Secret';
@@ -23,15 +23,28 @@ class App extends Component {
         mainComponent = <NotFound />;
     }
 
-    return (
-      <div className="App">
-        <header className="App-header">
+    return <div className="App">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          
           <h1 className="App-title">Welcome to Pedalhead, {this.props.name}!</h1>
-        </header>
+        </header> */}
+        <div class="jumbotron jumbotron-fluid" id="Jtron">
+          <div class="container">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 class="display-3" id="Pedalheader">
+              PEDALHEAD
+            </h1>
+            
+              <p class="lead">
+                An application for the busy cyclist who has more on their
+                mind than gears and wrenches...{' '}
+              </p>
+           
+          </div>
+        </div>
         {mainComponent}
-      </div>
-    );
+      </div>;
   }
 }
 
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,7 +4,9 @@ export default class NotFound extends Component {
   render() {
     return (
       <div>
-        <p>Page not found. If you entered a custom address, please verify it was correct. You can also navigate back to the home page to find the information you need.</p>
+        <p>
+          Page not found. If you entered a custom address, please verify it was correct. You can also navigate back to the <a href="/">home page</a> to find the information you need.
+        </p>
       </div>
     );
   }
