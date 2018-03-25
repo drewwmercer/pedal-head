d3c0b427117bf9f56251f2694f9576cd434c06aa
Drew Mercer <japanexpress@gmail.com>
2018-03-25 12:29:33 -0400

    Removing items from local storage

--- src/Auth.js
+++ src/Auth.js
@@ -19,27 +19,36 @@ export default class Auth {
   }
 
   login() {
-      this.auth0.authorize();
+    this.auth0.authorize();
   }
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResults) => {
-        if (authResults && authResults.accessToken && authResults.idToken) {
-            let expiresAt = JSON.stringify((authResults.expiresIn) * 1000 + new Date().getTime());
-            localStorage.setItem('access_token', authResults.accessToken);
-            localStorage.setItem('id_token', authResults.idToken);
-            localStorage.setItem('expires_at', expiresAt);
-            location.hash = '';
-            location.pathname = LOGIN_SUCCESS_PAGE;
-        } else if (err) {
-            location.pathname = LOGIN_FAILURE_PAGE;
-            console.log('Login error: ' + err)
-        }
+      if (authResults && authResults.accessToken && authResults.idToken) {
+        let expiresAt = JSON.stringify(
+          authResults.expiresIn * 1000 + new Date().getTime()
+        );
+        localStorage.setItem('access_token', authResults.accessToken);
+        localStorage.setItem('id_token', authResults.idToken);
+        localStorage.setItem('expires_at', expiresAt);
+        location.hash = '';
+        location.pathname = LOGIN_SUCCESS_PAGE;
+      } else if (err) {
+        location.pathname = LOGIN_FAILURE_PAGE;
+        console.log('Login error: ' + err);
+      }
     });
   }
 
   isAuthenticated() {
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    location.pathname = LOGIN_FAILURE_PAGE;
   }
 }
